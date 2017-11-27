c869c16535aa23cadffdd087203235b20d9fe0f5
Pratheek <sharma.pratheek59@gmail.com>
2017-11-28 00:04:56 +0530

    Seventh commit Remove redundancies and refacto code includes(_form,_messages) and we also added an Back Link to the From

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+	before_action :set_todo, only: [:edit,:update,:destroy,:show]
 	def new
 		@todo = Todo.new
 	end 
@@ -8,7 +9,7 @@ class TodosController < ApplicationController
 	end
 
 	def destroy
-		@todo = Todo.find(params[:id])
+		
 		@todo.destroy
 		flash[:notice] = "Todo was deleted successfully"
 		redirect_to todos_path
@@ -25,17 +26,17 @@ class TodosController < ApplicationController
 	end
 
 	def show 
-		@todo = Todo.find(params[:id])
+		
 	end
 
 
 	def edit
-		@todo = Todo.find(params[:id])
+		
 		
 	end
 
 	def update 
-		@todo = Todo.find(params[:id])
+		
 		if @todo.update(todo_params)
 			flash[:notice] = "Todo was updated successfully"
 			redirect_to todo_path(@todo)
@@ -49,4 +50,7 @@ class TodosController < ApplicationController
 		def todo_params
 			params.require(:todo).permit(:name 	, :description)
 		end	
+		def set_todo
+			@todo = Todo.find(params[:id])
+		end
 end
